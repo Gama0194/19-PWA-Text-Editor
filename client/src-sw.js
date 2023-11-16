b4624d4999416290a4b6f95e85a218dd87cb4683
Gama0194 <134178399+Gama0194@users.noreply.github.com>
2023-11-16 13:44:14 -0600

    added logic in database, still troubleshooting

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,14 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -20,19 +20,19 @@ const pageCache = new CacheFirst({
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
 registerRoute(
-  ({ request }) => request.destination === 'script' ||
-    request.destination === 'style' ||
-    request.destination === 'font',
+  ({ request }) =>
+    request.destination === "script" ||
+    request.destination === "style" ||
+    request.destination === "font",
   new CacheFirst({
-    cacheName: 'asset-cache',
+    cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
@@ -41,7 +41,5 @@ registerRoute(
         maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],
-  }),
+  })
 );
-
-
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,28 +1,27 @@
 const e = require("express");
 
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('hit')
-    console.log("event" + event)
-    event.preventDefault();
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
+window.addEventListener("beforeinstallprompt", (event) => {
+  console.log("hit");
+  console.log("event" + event);
+  event.preventDefault();
+  window.deferredPrompt = event;
+  butInstall.classList.toggle("hidden", false);
 });
 
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-        return;
-    }
-    promptEvent.prompt()
-    window.deferredPrompt = null;
-    butInstall.classList
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList;
 });
 
-
-window.addEventListener('appinstalled', (event) => {
-    console.log('ATE was installed.', event);
-    window.deferredPrompt = null;
+window.addEventListener("appinstalled", (event) => {
+  console.log("ATE was installed.", event);
+  window.deferredPrompt = null;
 });
