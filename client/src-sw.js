cc500c6a98488b7a80724c66df9fb256fe4e5945
Gama0194 <134178399+Gama0194@users.noreply.github.com>
2023-11-16 14:01:58 -0600

    added code to troubleshoot

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,9 +1,10 @@
-const { warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
-const { registerRoute } = require("workbox-routing");
-const { CacheableResponsePlugin } = require("workbox-cacheable-response");
-const { ExpirationPlugin } = require("workbox-expiration");
-const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+const { warmStrategyCache } = require('workbox-recipes');
+const { registerRoute } = require('workbox-routing');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { ExpirationPlugin } = require('workbox-expiration');
+
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -27,18 +28,12 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 registerRoute(
-  ({ request }) =>
-    request.destination === "script" ||
-    request.destination === "style" ||
-    request.destination === "font",
-  new CacheFirst({
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
     cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],
   })
